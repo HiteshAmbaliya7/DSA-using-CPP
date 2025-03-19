501e5fa8313d03483a6567b923935b208cc895ba
HiteshAmbaliya7 <hiteshambaliya2062004@gmail.com>
2025-03-19 13:33:09 +0530

    for loop

---
+++ loops/for_loop.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    for(int i=0;i<=5;i++){
+        cout<<i;
+    }
+    return 0;
+}
+
+/*
+this type of  loop use when we know which time loop exicute.
+it takes three arguments initialization , condition ,increment or decrement.
+step 1: initialization (int i =0)
+step 2: condition (i<=5)
+step 3: run the statement inside the loop(cout<<i)
+step 4: increment/decremet
+
+loop runs untill the given condition become false
+
+*/
